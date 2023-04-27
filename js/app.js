20fb5b35d854af82da60c3b88280fb92d58dc510
3vomanx <97153803+3vomanx@users.noreply.github.com>
2023-04-27 04:10:37 +0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    $(".alcobeve").click(function(){
+        $(".ab").slideToggle()
+        $(".alcobeve").toggleClass("fa-caret-right")
+    });
+    $(".basicfooditems").click(function(){
+        $(".bfi").slideToggle()
+        $(".basicfooditems").toggleClass("fa-caret-right")
+    });
+    $(".electronics").click(function(){
+        $(".elnics").slideToggle()
+        $(".electronics").toggleClass("fa-caret-right")
+    });
+    $(".appleproducts").click(function(){
+        $(".appleprod").slideToggle()
+        $(".appleproducts").toggleClass("fa-caret-right")
+    });
+    $(".sony").click(function(){
+        $(".sonyprod").slideToggle()
+        $(".sony").toggleClass("fa-caret-right")
+    });
+    $(".fruavege").click(function(){
+        $(".fruvege").slideToggle()
+        $(".fruavege").toggleClass("fa-caret-right")
+    });
+    $(".noabe").click(function(){
+        $(".nonabe").slideToggle()
+        $(".noabe").toggleClass("fa-caret-right")
+    });
+    $(".restau").click(function(){
+        $(".resto").slideToggle()
+        $(".restau").toggleClass("fa-caret-right")
+    });
+})
+
+$(document).on('click', '.categoryitem', function(){
+    localStorage.setItem("ClickedId", this.id);
+    window.location.href= 'Rankings.html'
+});
+
