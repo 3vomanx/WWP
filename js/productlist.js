03536cf6ed8e94b744321123e68ae96c2053dfbc
3vomanx <giorgigogidze39@gmail.com>
2023-04-29 02:15:35 +0400

    .

---
+++ js/productlist.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".main").click(function(){
+        $(".main").toggleClass("fa-bars")
+    })
+})
+
+$(document).ready(function(){
+    $(".main").click(function(){
+        $(".sidePanel").toggleClass("sidepushed")
+        $(".sidepanelbtn").toggleClass("sidebtnpushed")
+    })
+})
