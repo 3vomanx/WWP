4b941e07dddbffc405304d36fc76ad14f36a96a2
3vomanx <giorgigogidze39@gmail.com>
2023-04-29 02:46:33 +0400

    .

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(window).scroll(function(){
-    $('nav').toggleClass('scrolled', $(this).scrollTop() > 60)
+    $('nav').toggleClass('scrolled', $(this).scrollTop() > 10)
 });
 
 $(document).ready(function(){
