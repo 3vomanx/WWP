a46b65586bc479b83512b1ccd473d6a08342e920
3vomanx <giorgigogidze39@gmail.com>
2023-05-03 02:17:01 +0400

    .

--- js/home.js
+++ js/home.js
@@ -2,3 +2,20 @@ $(document).on('click', '.categorybox', function(){
     localStorage.setItem("ClickedId", this.id);
     window.location.href= 'Rankings.html'
 });
+
+$(window).scroll(function(){
+    $('.headerbar').toggleClass('scrolled', $(this).scrollTop() > 1)
+});
+
+$(document).ready(function(){
+    $(".main").click(function(){
+        $(".main").toggleClass("fa-bars")
+    })
+})
+
+$(document).ready(function(){
+    $(".main").click(function(){
+        $(".sidePanel").toggleClass("sidepushed")
+        $(".sidepanelbtn").toggleClass("sidebtnpushed")
+    })
+})
